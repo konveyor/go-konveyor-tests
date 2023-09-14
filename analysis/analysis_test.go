154fe72b330ca739a650b655aa58bfa8cf6c9aa2
Maayan Hadasi <mguetta@redhat.com>
2023-09-14 08:52:31 +0300

    update
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>

--- analysis/analysis_test.go
+++ analysis/analysis_test.go
@@ -16,7 +16,7 @@ import (
 
 // Test application analysis
 func TestApplicationAnalysis(t *testing.T) {
-	// Find right test cases for given Tier (include Tier0 always).
+	// Find right test cases for given Tier.
 	testCases := Tier0TestCases
 	_, tier1 := os.LookupEnv("TIER1")
 	if tier1 {
@@ -73,9 +73,12 @@ func TestApplicationAnalysis(t *testing.T) {
 			if len(tc.Targets) > 0 {
 				taskData.Targets = tc.Targets
 			}
+<<<<<<< HEAD
 			if len(tc.Labels.Included) > 0 || len(tc.Labels.Excluded) > 0 {
 				taskData.Rules.Labels = tc.Labels
 			}
+=======
+>>>>>>> acba82e (update)
 			if tc.Rules.Path != "" { // TODO: better rules handling
 				taskData.Rules = tc.Rules
 			}
