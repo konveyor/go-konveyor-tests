ac53206e49b97aaef0f866a0814c8c8530f4c8d2
Marek Aufart <maufart@redhat.com>
2023-09-15 16:08:50 +0200

    Fix Tackle testapp analysis results
    Current analysis report sets all Incidents lines to 0 and doesn't add
    Tags to the application.
    
    Opening this PR to track progress on fixing it.
    
    Related to https://github.com/konveyor/go-konveyor-tests/issues/22
    
    Signed-off-by: Marek Aufart <maufart@redhat.com>

--- analysis/tc_tackle_testapp_public.go
+++ analysis/tc_tackle_testapp_public.go
@@ -33,12 +33,12 @@ var TackleTestappPublic = TC{
 				Incidents: []api.Incident{
 					{
 						File:    "/addon/source/tackle-testapp-public/target/classes/persistence.properties",
-						Line:    0,
+						Line:    0,	// Fix.
 						Message: "When migrating environments, hard-coded IP addresses may need to be modified or eliminated.",
 					},
 					{
 						File:    "/addon/source/tackle-testapp-public/src/main/resources/persistence.properties",
-						Line:    0,
+						Line:    0,	// Fix.
 						Message: "When migrating environments, hard-coded IP addresses may need to be modified or eliminated.",
 					},
 				},
@@ -52,37 +52,37 @@ var TackleTestappPublic = TC{
 				Incidents: []api.Incident{
 					{
 						File:    "/addon/source/tackle-testapp-public/src/main/java/io/konveyor/demo/ordermanagement/config/PersistenceConfig.java",
-						Line:    0,
+						Line:    0,	// Fix.
 						Message: "An application running inside a container could lose access to a file in local storage.. Recommendations. The following recommendations depend on the function of the file in local storage:. * Logging: Log to standard output and use a centralized log collector to analyze the logs.. * Caching: Use a cache backing service.. * Configuration: Store configuration settings in environment variables so that they can be updated without code changes.. * Data storage: Use a database backing service for relational data or use a persistent data storage system.. * Temporary data storage: Use the file system of a running container as a brief, single-transaction cache.",
 					},
 					{
 						File:    "/addon/source/tackle-testapp-public/src/main/java/io/konveyor/demo/ordermanagement/config/PersistenceConfig.java",
-						Line:    0,
+						Line:    0,	// Fix.
 						Message: "An application running inside a container could lose access to a file in local storage.. Recommendations. The following recommendations depend on the function of the file in local storage:. * Logging: Log to standard output and use a centralized log collector to analyze the logs.. * Caching: Use a cache backing service.. * Configuration: Store configuration settings in environment variables so that they can be updated without code changes.. * Data storage: Use a database backing service for relational data or use a persistent data storage system.. * Temporary data storage: Use the file system of a running container as a brief, single-transaction cache.",
 					},
 					{
 						File:    "/addon/source/tackle-testapp-public/src/main/java/io/konveyor/demo/ordermanagement/config/PersistenceConfig.java",
-						Line:    0,
+						Line:    0,	// Fix.
 						Message: "An application running inside a container could lose access to a file in local storage.. Recommendations. The following recommendations depend on the function of the file in local storage:. * Logging: Log to standard output and use a centralized log collector to analyze the logs.. * Caching: Use a cache backing service.. * Configuration: Store configuration settings in environment variables so that they can be updated without code changes.. * Data storage: Use a database backing service for relational data or use a persistent data storage system.. * Temporary data storage: Use the file system of a running container as a brief, single-transaction cache.",
 					},
 					{
 						File:    "/addon/source/tackle-testapp-public/src/main/java/io/konveyor/demo/ordermanagement/config/PersistenceConfig.java",
-						Line:    0,
+						Line:    0,	// Fix.
 						Message: "An application running inside a container could lose access to a file in local storage.. Recommendations. The following recommendations depend on the function of the file in local storage:. * Logging: Log to standard output and use a centralized log collector to analyze the logs.. * Caching: Use a cache backing service.. * Configuration: Store configuration settings in environment variables so that they can be updated without code changes.. * Data storage: Use a database backing service for relational data or use a persistent data storage system.. * Temporary data storage: Use the file system of a running container as a brief, single-transaction cache.",
 					},
 					{
 						File:    "/addon/source/tackle-testapp-public/src/main/java/io/konveyor/demo/ordermanagement/config/PersistenceConfig.java",
-						Line:    0,
+						Line:    0,	// Fix.
 						Message: "An application running inside a container could lose access to a file in local storage.. Recommendations. The following recommendations depend on the function of the file in local storage:. * Logging: Log to standard output and use a centralized log collector to analyze the logs.. * Caching: Use a cache backing service.. * Configuration: Store configuration settings in environment variables so that they can be updated without code changes.. * Data storage: Use a database backing service for relational data or use a persistent data storage system.. * Temporary data storage: Use the file system of a running container as a brief, single-transaction cache.",
 					},
 					{
 						File:    "/addon/source/tackle-testapp-public/src/main/java/io/konveyor/demo/ordermanagement/config/PersistenceConfig.java",
-						Line:    0,
+						Line:    0,	// Fix.
 						Message: "An application running inside a container could lose access to a file in local storage.. Recommendations. The following recommendations depend on the function of the file in local storage:. * Logging: Log to standard output and use a centralized log collector to analyze the logs.. * Caching: Use a cache backing service.. * Configuration: Store configuration settings in environment variables so that they can be updated without code changes.. * Data storage: Use a database backing service for relational data or use a persistent data storage system.. * Temporary data storage: Use the file system of a running container as a brief, single-transaction cache.",
 					},
 					{
 						File:    "/addon/source/tackle-testapp-public/src/main/java/io/konveyor/demo/ordermanagement/config/PersistenceConfig.java",
-						Line:    0,
+						Line:    0,	// Fix.
 						Message: "An application running inside a container could lose access to a file in local storage.. Recommendations. The following recommendations depend on the function of the file in local storage:. * Logging: Log to standard output and use a centralized log collector to analyze the logs.. * Caching: Use a cache backing service.. * Configuration: Store configuration settings in environment variables so that they can be updated without code changes.. * Data storage: Use a database backing service for relational data or use a persistent data storage system.. * Temporary data storage: Use the file system of a running container as a brief, single-transaction cache.",
 					},
 				},
@@ -96,13 +96,14 @@ var TackleTestappPublic = TC{
 				Incidents: []api.Incident{
 					{
 						File:    "/addon/source/tackle-testapp-public/.git/hooks/fsmonitor-watchman.sample",
-						Line:    0,
+						Line:    0,	// Fix.
 						Message: "This file system path is Microsoft Windows platform dependent. It needs to be replaced with a Linux-style path.",
 					},
 				},
 			},
 		},
 	},
-	// Tags are empty right now.
-	AnalysisTags: []api.Tag{},
+	AnalysisTags: []api.Tag{
+		// Fix.
+	},
 }
