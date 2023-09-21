125cf102b49b8cabfbd60e2d64a75cce6c050f18
Maayan Hadasi <mguetta@redhat.com>
2023-09-21 14:20:05 +0300

    Modifying metrics so tests can be executed against OpenShift cluster
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>

--- config/configuration.go
+++ config/configuration.go
@@ -1,6 +1,7 @@
 package config
 
 type Configuration struct {
+	KUBECONFIG    string `env:"KUBECONFIG"`
 	JIRA_USERNAME string `env:"JIRA_USERNAME"`
 	JIRA_PASSWORD string `env:"JIRA_PASSWORD"`
 	JIRA_URL      string `env:"JIRA_URL"`
