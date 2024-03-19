f2eaffdcce4e5eefc1536ed5691efb982780a0b9
Maayan Hadasi <60384172+mguetta1@users.noreply.github.com>
2024-03-19 10:09:07 +0200

    Upload binary analysis test (#101)
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>

--- analysis/test_cases.go
+++ analysis/test_cases.go
@@ -5,6 +5,7 @@ package analysis
 var Tier0TestCases = []TC{
 	TackleTestappPublicWithDeps,
 	TackleTestappPublicPackageFilter,
+	AdministracionEfectivo,
 }
 
 // Tier 1 Analysis test cases - should work.
