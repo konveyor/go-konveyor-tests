861dd3e28aee8b747c1609c26ac100db0558606d
Shawn Hurley <shawn@hurley.page>
2024-07-08 20:02:46 -0400

    Moving Tomcat analysis TC to TIER0 (#125) (#157)
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>
    Co-authored-by: Maayan Hadasi <60384172+mguetta1@users.noreply.github.com>

--- analysis/test_cases.go
+++ analysis/test_cases.go
@@ -6,6 +6,7 @@ var Tier0TestCases = []TC{
 	TackleTestappPublicWithDeps,
 	TackleTestappPublicPackageFilter,
 	AcmeairWebapp,
+	Tomcat,
 }
 
 // Tier 1 Analysis test cases - should work.
@@ -18,7 +19,6 @@ var Tier1TestCases = []TC{
 // List of applications with expected analysis outputs.
 var Tier2TestCases = []TC{
 	AdministracionEfectivo,
-	Tomcat,
 	TackleTestappPublic,
 	Daytrader,
 	PetclinicHazelcast,
