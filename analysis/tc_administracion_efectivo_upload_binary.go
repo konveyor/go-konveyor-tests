3a43060118e3dc544d42a56ab2f33dd63c15321c
Maayan Hadasi <mguetta@redhat.com>
2024-07-29 15:12:15 +0300

    Editing expected issues and tags
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>

--- analysis/tc_administracion_efectivo_upload_binary.go
+++ analysis/tc_administracion_efectivo_upload_binary.go
@@ -18,21 +18,31 @@ var AdministracionEfectivo = TC{
 	Binary:   true,
 	Artifact: "/binary/administracion_efectivo.ear",
 	Analysis: api.Analysis{
-		Effort: 1591,
+		Effort: 36,
 		Issues: []api.Issue{
-			{
-				Category:    "mandatory",
-				Description: "Java native libraries (JNI, JNA)",
-				Effort:      7,
-				RuleSet:     "cloud-readiness",
-				Rule:        "jni-native-code-00000",
-			},
 			{
 				Category:    "mandatory",
 				Description: "File system - Java IO",
 				Effort:      1,
 				RuleSet:     "cloud-readiness",
 				Rule:        "local-storage-00001",
+				Incidents: []api.Incident{
+					{
+						File:     "/shared/bin/java-project/src/main/java/org/primefaces/component/export/XMLExporter.java",
+						Line:     28,
+						CodeSnip: "PrintWriter writer = new PrintWriter(osw);",
+					},
+					{
+						File:     "/shared/bin/java-project/src/main/java/org/primefaces/component/imagecropper/ImageCropperRenderer.java",
+						Line:     145,
+						CodeSnip: `outputImage = ImageIO.read(new File(servletContext.getRealPath("") + imagePath));`,
+					},
+					{
+						File:     "/shared/bin/java-project/src/main/java/org/primefaces/webapp/filter/FileUploadFilter.java",
+						Line:     48,
+						CodeSnip: "diskFileItemFactory.setRepository(new File(this.uploadDir));",
+					},
+				},
 			},
 			{
 				Category:    "mandatory",
@@ -40,24 +50,43 @@ var AdministracionEfectivo = TC{
 				Effort:      1,
 				RuleSet:     "cloud-readiness",
 				Rule:        "local-storage-00002",
+				Incidents: []api.Incident{
+					{
+						File:     "/shared/bin/java-project/src/main/java/org/primefaces/component/captcha/Captcha.java",
+						Line:     95,
+						CodeSnip: `URL url = new URL("http://api-verify.recaptcha.net/verify");`,
+					},
+					{
+						File:     "/shared/bin/java-project/src/main/java/org/primefaces/component/feedreader/FeedInput.java",
+						Line:     14,
+						CodeSnip: "URL feedSource = new URL(url);",
+					},
+					{
+						File:     "/shared/bin/java-project/src/main/java/org/primefaces/component/imagecropper/ImageCropperRenderer.java",
+						Line:     141,
+						CodeSnip: "URL url = new URL(imagePath);",
+					},
+				},
 			},
+		},
+		Dependencies: []api.TechDependency{
 		},
 	},
 	AnalysisTags: []api.Tag{
 		{Name: "Servlet", Category: api.Ref{Name: "HTTP"}},
 		{Name: "EAR Deployment", Category: api.Ref{Name: "Other"}},
-		{Name: "JNI", Category: api.Ref{Name: "Other"}},
 		{Name: "Mail", Category: api.Ref{Name: "Other"}},
+		{Name: "JPA named queries", Category: api.Ref{Name: "Persistence"}},
 		{Name: "Spring Scheduled", Category: api.Ref{Name: "Processing"}},
 		{Name: "Mail", Category: api.Ref{Name: "Connect"}},
 		{Name: "Servlet", Category: api.Ref{Name: "Connect"}},
 		{Name: "EAR Deployment", Category: api.Ref{Name: "Connect"}},
-		{Name: "JNI", Category: api.Ref{Name: "Connect"}},
-		{Name: "JNI", Category: api.Ref{Name: "Java EE"}},
-		{Name: "Servlet", Category: api.Ref{Name: "Java EE"}},
-		{Name: "EAR Deployment", Category: api.Ref{Name: "Java EE"}},
-		{Name: "Mail", Category: api.Ref{Name: "Java EE"}},
-		{Name: "Spring Scheduled", Category: api.Ref{Name: "Embedded"}},
 		{Name: "Spring Scheduled", Category: api.Ref{Name: "Execute"}},
+		{Name: "JPA named queries", Category: api.Ref{Name: "Store"}},
+		{Name: "Spring Scheduled", Category: api.Ref{Name: "Embedded"}},
+		{Name: "JPA named queries", Category: api.Ref{Name: "Java EE"}},
+		{Name: "Mail", Category: api.Ref{Name: "Java EE"}},
+		{Name: "EAR Deployment", Category: api.Ref{Name: "Java EE"}},
+		{Name: "Servlet", Category: api.Ref{Name: "Java EE"}},
 	},
 }
