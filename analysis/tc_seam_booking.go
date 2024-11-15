c7d2af9c8c5b18bc936e16032e14677928ed204d
Nandini Chandra <nachandr@redhat.com>
2024-11-15 13:10:27 -0600

    Update Analysis issues and incidents
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- analysis/tc_seam_booking.go
+++ analysis/tc_seam_booking.go
@@ -20,7 +20,7 @@ var SeamBooking = TC{
 	Labels: addon.Labels{
 		Included: []string{
 			"konveyor.io/target=cloud-readiness",
-			"konveyor.io/target=jakarta-ee",
+			"konveyor.io/target=eap",
 			},
 	},
 	ReportContent: map[string][]string{
@@ -33,37 +33,273 @@ var SeamBooking = TC{
 		Effort: 3,
 		Issues: []api.Issue{
 			{
-				Category:    "potential",
-				Description: "The groupId `javax` has been replaced by `jakarta` in JBoss EAP 7.3, or later",
+				Category:    "mandatory",
+				Description: "Hibernate 5.3 - default_schema or default_catalog must be defined or set jdbc_metadata_extraction_strategy",
 				Effort:       1,
 				RuleSet:     "eap7/weblogic/tests/data",
-				Rule:        "maven-javax-to-jakarta-00004",
-				Name:        "Seam booking 5.2 JLYDS",
+				Rule:        "hibernate51-53-01000",
 				Incidents: []api.Incident{
 					{
-						File:    "/shared/source/windup/test-files/seam-booking-5.2/pom.xml",
-						Line:    24,
-						Message: "If you migrate your application to JBoss EAP 7.3, or later, and want to ensure its Maven building, running or testing works as expected, use instead the Jakarta EE dependency - groupId jakarta..",
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/resources/META-INF/persistence.xml",
+						Line:    7,
+						Message: "Define `hibernate.default_schema` or `hibernate.default_catalog` (whichever is used by the selected dialect), or, alternatively, set `hibernate.hbm2ddl.jdbc_metadata_extraction_strategy=individually`.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/resources/META-INF/persistence.xml",
+						Line:    9,
+						Message: "Define `hibernate.default_schema` or `hibernate.default_catalog` (whichever is used by the selected dialect), or, alternatively, set `hibernate.hbm2ddl.jdbc_metadata_extraction_strategy=individually`.",
 					},
 				},
 			},
 			{
-				Category: "potential",
-				Description: "web.xml element references a javax-prefixed class name",
+				Category: "mandatory",
+				Description: "JSF Seam 2 UI control s:button",
 				Effort: 1,
-				RuleSet:     "eap8/eap7",
-				Rule:        "javax-to-jakarta-servlet-00130",
-				Name: "Maven POM (pom.xml)",
+				RuleSet:     "eap6/java-ee/seam",
+				Rule:        "seam-ui-jsf-00001-01",
 				Incidents: []api.Incident{
 					{
-						File:    "/shared/source/windup/test-files/seam-booking-5.2/resources/WEB-INF/web.xml",
-						Line:    40,
-						Message: "web.xml element references a javax-prefixed class name",
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/book.xhtml",
+						Line:    94,
+						Message: "Seam UI's `<s:button>` JSF control should be replaced by `<h:button>`.\n" +
+			 " There are differences in attributes, for example the _action_ attribute maps to _outcome_ and there is no _propagation_ attribute.",
 					},
 					{
-						File:    "/shared/source/windup/test-files/seam-booking-5.2/resources/WEB-INF/web.xml",
-						Line:    51,
-						Message: "web.xml element references a javax-prefixed class name",
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/confirm.xhtml",
+						Line:    42,
+						Message: "Seam UI's `<s:button>` JSF control should be replaced by `<h:button>`.\n" +
+						" There are differences in attributes, for example the _action_ attribute maps to _outcome_ and there is no _propagation_ attribute.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/password.xhtml",
+						Line:    36,
+						Message: "Seam UI's `<s:button>` JSF control should be replaced by `<h:button>`.\n" +
+						" There are differences in attributes, for example the _action_ attribute maps to _outcome_ and there is no _propagation_ attribute.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/register.xhtml",
+						Line:    81,
+						Message: "Seam UI's `<s:button>` JSF control should be replaced by `<h:button>`.\n" +
+						" There are differences in attributes, for example the _action_ attribute maps to _outcome_ and there is no _propagation_ attribute",
+					},
+				},
+			},
+			{
+				Category: "mandatory",
+				Description: "JSF Seam 2 UI control s:convertDateTime",
+				Effort: 1,
+				RuleSet:     "eap6/java-ee/seam",
+				Rule:        "seam-ui-jsf-01005",
+			},
+			{
+				Category: "mandatory",
+				Description: "JSF Seam 2 UI control s:decorate",
+				Effort: 3,
+				RuleSet:     "eap6/java-ee/seam",
+				Rule:        "seam-ui-jsf-01023",
+				Incidents: []api.Incident{
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/book.xhtml",
+						Line:    29,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/book.xhtml",
+						Line:    34,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/book.xhtml",
+						Line:    39,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/book.xhtml",
+						Line:    48,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/book.xhtml",
+						Line:    56,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/book.xhtml",
+						Line:    63,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/book.xhtml",
+						Line:    70,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/confirm.xhtml",
+						Line:    21,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/confirm.xhtml",
+						Line:    28,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/confirm.xhtml",
+						Line:    33,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/confirm.xhtml",
+						Line:    38,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/hotelview.xhtml",
+						Line:    9,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/hotelview.xhtml",
+						Line:    14,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/hotelview.xhtml",
+						Line:    19,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/hotelview.xhtml",
+						Line:    24,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/hotelview.xhtml",
+						Line:    29,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/hotelview.xhtml",
+						Line:    34,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/hotelview.xhtml",
+						Line:    39,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/password.xhtml",
+						Line:    27,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/password.xhtml",
+						Line:    32,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/register.xhtml",
+						Line:    58,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/register.xhtml",
+						Line:    65,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/register.xhtml",
+						Line:    72,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/register.xhtml",
+						Line:    77,
+						Message: "There is no direct mapping for <s:decorate> in JSF UI controls, but you can achieve the same functionality by using the UIInputContainer and a composite container, both of which are demonstrated in the Open18 migration example input.xhtml file.",
+					},
+				},
+			},
+			{
+				Category: "mandatory",
+				Description: "JSF Seam 2 UI control s:label",
+				Effort: 1,
+				RuleSet:     "eap6/java-ee/seam",
+				Rule:        "seam-ui-jsf-01026",
+				Incidents: []api.Incident{
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/edit.xhtml",
+						Line:    11,
+						Message: "There is no direct mapping for <s:label> in JSF UI controls, but <h:outputLabel> is similar.",
+					},
+				},
+			},
+			{
+				Category: "mandatory",
+				Description: "JSF Seam 2 UI control s:link",
+				Effort: 1,
+				RuleSet:     "eap6/java-ee/seam",
+				Rule:        "seam-ui-jsf-01000",
+				Incidents: []api.Incident{
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/home.xhtml",
+						Line:    10,
+						Message: "Seam UI's `<s:link>` should be replaced by `<h:link>`.\n" +
+						" There are differences in attributes, for example the _action_ attribute maps to _outcome_ and there is no _propagation_ attribute.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/main.xhtml",
+						Line:    68,
+						Message: "Seam UI's `<s:link>` should be replaced by `<h:link>`.\n" +
+						" There are differences in attributes, for example the _action_ attribute maps to _outcome_ and there is no _propagation_ attribute.",
+					},
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/template.xhtml",
+						Line:    13,
+						Message: "Seam UI's `<s:link>` should be replaced by `<h:link>`.\n"+
+						" There are differences in attributes, for example the _action_ attribute maps to _outcome_ and there is no _propagation_ attribute.",
+					},
+				},
+			},
+			{
+				Category: "mandatory",
+				Description: "JSF Seam 2 UI control s:message",
+				Effort: 1,
+				RuleSet:     "eap6/java-ee/seam",
+				Rule:        "seam-ui-jsf-01027",
+				Incidents: []api.Incident{
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/edit.xhtml",
+						Line:    18,
+						Message: `Use <h:message for="name" errorClass="invalid"> or Richfaces <rich:message>.`,
+					},
+				},
+			},
+			{
+				Category: "mandatory",
+				Description: "JSF Seam 2 UI control s:span",
+				Effort: 1,
+				RuleSet:     "eap6/java-ee/seam",
+				Rule:        "seam-ui-jsf-01025",
+				Incidents: []api.Incident{
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/edit.xhtml",
+						Line:    11,
+						Message: "There is no direct mapping for <s:span> in JSF UI controls, but you can achieve a similar effect by using <h:panelGroup> or a <ui:fragment> with a span element.",
+					},
+				},
+			},
+			{
+				Category: "mandatory",
+				Description: "JSF Seam 2 UI control s:validateAll",
+				Effort: 1,
+				RuleSet:     "eap6/java-ee/seam",
+				Rule:        "seam-ui-jsf-01022",
+				Incidents: []api.Incident{
+					{
+						File:    "/shared/source/windup/test-files/seam-booking-5.2/view/edit.xhtml",
+						Line:    14,
+						Message: "There is no direct mapping for <s:validateAll> in JSF UI controls, but you can achieve a similar effect by using <f:validateBean> or Richfaces <rich:validator>.	",
 					},
 				},
 			},
