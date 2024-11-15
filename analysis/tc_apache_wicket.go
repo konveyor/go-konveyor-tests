467be5fd1b4b8c844296ef590d881cab26e75e08
Nandini Chandra <nachandr@redhat.com>
2024-11-15 17:16:31 -0600

    Update Incidents
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- analysis/tc_apache_wicket.go
+++ analysis/tc_apache_wicket.go
@@ -30,6 +30,18 @@ var ApacheWicket = TC{
 				Description: "Hardcoded IP Address",
 				RuleSet: "discovery-rules",
 				Rule: "hardcoded-ip-address",
+				Incidents: []api.Incident{
+					{
+						File:    "/shared/source/windup-sample-apps/test-files/src_example/org/apache/wicket/protocol/http/mock/MockHttpServletRequest.java",
+						Line:    51,
+						Message: "When migrating environments, hard-coded IP addresses may need to be modified or eliminated.",
+					},
+					{
+						File:    "/shared/source/windup-sample-apps/test-files/src_example/org/apache/wicket/protocol/http/mock/MockHttpServletRequest.java",
+						Line:    584,
+						Message: "When migrating environments, hard-coded IP addresses may need to be modified or eliminated.",
+					},
+				},
 			},
 			{
 				Category: "mandatory",
@@ -37,6 +49,13 @@ var ApacheWicket = TC{
 				Description: "Local HTTP Calls",
 				RuleSet: "cloud-readiness",
 				Rule: "localhost-http-00001",
+				Incidents: []api.Incident{
+					{
+						File:    "/shared/source/windup-sample-apps/test-files/src_example/org/apache/wicket/protocol/http/mock/MockHttpServletRequest.java",
+						Line:    362,
+						Message: "The app is trying to access local resource by HTTP, please try to migrate the resource to cloud",
+					},
+				},
 			},
 		},
 	},
