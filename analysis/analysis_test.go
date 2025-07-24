1516ed07e383e337deec0151be31048f58bb86f4
Maayan Hadasi <60384172+mguetta1@users.noreply.github.com>
2025-07-24 09:55:11 +0300

    Print CodeSnip if analysis fails on incident line (#307)
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>

--- analysis/analysis_test.go
+++ analysis/analysis_test.go
@@ -273,7 +273,7 @@ func verifyAnalysis(t TaskTest, tc TC, debug bool) {
 						t.Errorf("\nDifferent incident.File error. Got %+v\nExpected %+v.\n\n", gotInc.File, expectedInc.File)
 					}
 					if gotInc.Line != expectedInc.Line {
-						t.Errorf("\nDifferent incident.Line error. Got %+v\nExpected %+v.\n\n", gotInc.Line, expectedInc.Line)
+						t.Errorf("\nDifferent incident.Line error. Got %+v\nExpected %+v.\nCodeSnip: %s\n\n", gotInc.Line, expectedInc.Line, gotInc.CodeSnip)
 					}
 					if !strings.HasPrefix(gotInc.Message, expectedInc.Message) {
 						t.Errorf("\nDifferent incident.Message error. Got %+v\nExpected %+v.\n\n", gotInc.Message, expectedInc.Message)
