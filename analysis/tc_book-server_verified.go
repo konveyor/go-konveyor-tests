6528dccf79ff4d7d2d5147809ba102186c2f1d08
Maayan Hadasi <mguetta@redhat.com>
2025-11-03 19:13:23 +0200

    Adding comment in analysis test case
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>

--- analysis/tc_book-server_verified.go
+++ analysis/tc_book-server_verified.go
@@ -3,4 +3,6 @@ package analysis
 var BookServerVerified = TC{
 	Name: "book-server_deps",
 	Task: Analyze,
+
+	// For more information on test case data, see the README.md file in this directory.
 }
